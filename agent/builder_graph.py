abdeee3235a7e3c08c31ddabe98a92da53c2cf0c
biplavpoudel <biplavpoudel764@gmail.com>
2025-07-15 14:19:49 +0545

    refactor: refactored functions into a singular AssistantAgent class

--- agent/builder_graph.py
+++ agent/builder_graph.py
@@ -2,7 +2,7 @@
 
 import httpx
 import asyncio
-
+from pathlib import Path
 import logging
 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -26,63 +26,77 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger(__name__)
 
-with open("./prompts/system_prompt.txt", "r", encoding="utf-8") as f:
-    ASSISTANT_SYSTEM_PROMPT_BASE = f.read()
+SYSTEM_PROMPT_PATH = Path("./prompts/system_prompt.txt")
 
 
 class GraphProcessingState(BaseModel):
     messages: Annotated[list[BaseMessage], add_messages] = Field(default_factory=list)
     prompts: str = Field(default_factory=str, description="Prompts to be used by the agent")
-    # tools_enabled: dict = Field(default_factory=dict, description="Tools enabled for the agent")
 
+class AssistantAgent:
+    def __init__(self, llm_provider:str, system_prompt_path: Path = SYSTEM_PROMPT_PATH):
+        """
+        Attributes:
+            llm_provider(str): ollama (for development); gemini, gpt-4o, grok, etc. (for production)
+            system_prompt_path(Path): Path object representing the system prompt
+        """
+        self.llm_provider = llm_provider
+        self.system_prompt = system_prompt_path.read_text(encoding="utf-8")
+        self.llm = self.init_model()
+        self.client = self.init_mcp_client()
+        self.tools = []
 
-async def build_workflow(llm_provider="ollama") -> CompiledStateGraph:
-    """
-    Building LangGraph client that uses MCP Servers
-    :param llm_provider: ollama (for development); gemini, gpt-4o, grok, etc. (for production)
-    """
+    def init_model(self):
+        """Initializing the LLM model for agentic workflow
+        """
 
-    # Selecting LLM providers
-    try:
-        if llm_provider == "ollama":
-            llm = ChatOllama(model="qwen3:8b", temperature=0.2)
-        elif llm_provider == "gemini":
-            llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.1)
-        elif llm_provider == "openai":
-            llm = ChatOpenAI(model="gpt-4o",
-                             temperature=0,
-                             max_retries=3,
-                             timeout=60)
-        else:
+        provider = self.llm_provider
+        try:
+            if provider == "ollama":
+                return ChatOllama(model="qwen3:8b", temperature=0.2)
+            elif provider == "gemini":
+                return ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.1)
+            elif provider == "openai":
+                return ChatOpenAI(model="gpt-4o",
+                                 temperature=0,
+                                 max_retries=3,
+                                 timeout=60)
+            else:
+                raise ValueError(f"Unknown LLM provider: {provider}")
+        except httpx.ConnectError as e:
+            print(f"Ollama not running in background!. {str(e)}")
+        except Exception as e:
+            print(f"LLM initialization failed! {str(e)}")
 
-            raise ValueError(f"Unknown LLM provider: {llm_provider}")
-    except httpx.ConnectError as e:
-        print(f"Ollama not running in background!. {str(e)}")
-    except Exception as e:
-        print(f"LLM initialization failed! {str(e)}")
-
-    # Initializing MCP Client with MCP servers connections (e.g. mcp_server.py)
-    client = MultiServerMCPClient(
-        {
-            "mcp": {
-                "command": "python",
-                "args": ["./agent/mcp_server.py"],
-                "transport": "stdio",
+    @staticmethod
+    def init_mcp_client() -> MultiServerMCPClient:
+        """Initializing MCP Client with MCP servers connections (e.g. mcp_server.py)
+        """
+        client = MultiServerMCPClient(
+            {
+                "mcp": {
+                    "command": "python",
+                    "args": ["./agent/mcp_server.py"],
+                    "transport": "stdio",
+                }
             }
-        }
-    )
+        )
+        return client
 
-    assistant_tools = await client.get_tools()
-    for tool in assistant_tools:
-        logging.info(f"{tool}\n")
+    async def setup_tools(self):
+        self.tools = await self.client.get_tools()
+        for tool in self.tools:
+            logger.info(f"Loaded Tool: {tool}")
 
-    # Creating LangGraph Nodes
-    async def assistant_node(state: GraphProcessingState, config=None):
-        assistant_model = llm.bind_tools(assistant_tools)
+
+    async def assistant_node(self, state: GraphProcessingState, config=None):
+        """ Creating Assistant LangGraph Nodes
+        """
+        assistant_model = self.llm.bind_tools(self.tools)
         if state.prompts:
-            final_prompt = "\n".join([state.prompt, ASSISTANT_SYSTEM_PROMPT_BASE])
+            final_prompt = "\n".join([state.prompt, self.system_prompt])
         else:
-            final_prompt = ASSISTANT_SYSTEM_PROMPT_BASE
+            final_prompt = self.system_prompt
         # creating a chat prompt template with system messages along with user messages using placeholder
         prompts = ChatPromptTemplate.from_messages(
             [
@@ -94,6 +108,7 @@ async def build_workflow(llm_provider="ollama") -> CompiledStateGraph:
         response = await sequence.ainvoke({"messages": state.messages}, config=config)
         return {"messages": response}
 
+    @staticmethod
     def tools_condition_edge(state: GraphProcessingState):
         # similar to tools_condition from langgraph.prebuilt.tool_node, but added logger for debugging
         # routes to ToolNode if tools called in the last message, else ENDS
@@ -103,34 +118,42 @@ async def build_workflow(llm_provider="ollama") -> CompiledStateGraph:
             return "tools"
         return END
 
-    # async def graph_workflow() -> CompiledStateGraph:
-    # Initializing graph with state
-    builder = StateGraph(GraphProcessingState)
-    # Adding nodes to the graph
-    builder.add_node("assistant", assistant_node)
-    builder.add_node("tools", ToolNode(assistant_tools))
-    # Adding LangGraph edges
-    builder.add_edge(START, "assistant")
-    builder.add_conditional_edges(
-        "assistant",
-        tools_condition_edge,
-    )
-    builder.add_edge("tools", "assistant")
-    builder.add_edge("assistant", END)
-    # Compiling the graph
-    return builder.compile()
+    async def build_workflow(self) -> CompiledStateGraph:
+        """ Building Language Workflow
+        """
+        await self.setup_tools()
+        builder = StateGraph(GraphProcessingState)
+        # Adding nodes to the graph
+        builder.add_node("assistant", self.assistant_node)
+        builder.add_node("tools", ToolNode(self.tools))
+        # Adding LangGraph edges
+        builder.add_edge(START, "assistant")
+        builder.add_conditional_edges(
+            "assistant",
+            self.tools_condition_edge,
+        )
+        builder.add_edge("tools", "assistant")
+        builder.add_edge("assistant", END)
+        # Compiling the graph
+        return builder.compile()
+
+
+    async def ask(self, question:str):
+        builder_graph = await self.build_workflow()
+
+        # Create an initial state instance that conforms to GraphProcessingState
+        initial_state = GraphProcessingState(
+            messages=[HumanMessage(content=question)],
+            prompts=""
+        )
+        return await builder_graph.ainvoke(initial_state)
 
 
 async def main():
-    builder_graph = await build_workflow(llm_provider="ollama")
-    question = "what are the tool names from the mcp servers?"
-    # Create an initial state instance that conforms to GraphProcessingState
-    initial_state = GraphProcessingState(
-        messages=[HumanMessage(content=question)],
-        prompts=""
-    )
-    graph_response = await builder_graph.ainvoke(initial_state)
-    print(graph_response["messages"][-1].content)
+    agent = AssistantAgent(llm_provider="ollama")
+    response = await agent.ask("what are the tool names from the mcp servers?")
+    print("\nFinal Response:\n", response["messages"][-1].content)
+
 
 
 if __name__ == "__main__":
